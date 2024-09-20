dd72b755e575f8635c8aa5bbd8909e3685822729
Stephany Resque <resquestephany@gmail.com>
2024-09-20 11:35:21 -0300

    rev ok

--- C/rev_recursao/rec.c
+++ C/rev_recursao/rec.c
@@ -23,10 +23,58 @@ int naturais(int tam){
     printf("%d ", tam);
 }
 
-// int fibonatti(int n){
+int fibonatti(int n){
 
-//     return ()
-// }
+    if( n == 0){
+
+        return 0;
+    }else if( n == 1){
+
+        return 1;
+    }else{
+        return fibonatti(n - 1) + fibonatti(n - 2);
+    }
+
+
+}
+
+int Ackermann(int m, int n){
+
+    if( m == 0){
+
+        return (n+1);
+    }else if(n==0 && m>0){
+
+        return Ackermann(m-1, 1);
+    }else if(n > 0 && m > 0){
+
+        return Ackermann(m-1, Ackermann(m, n-1));
+    }
+}
+
+int inverso(int n, int j){
+
+    if(n==0){
+
+        return j;
+    }
+
+    j = (j * 10) + n%10;
+    return inverso(n/10, j);
+}
+
+int binario(int n){
+
+    if(n == 0){
+
+        return 0;
+    }else{
+
+        binario(n/2);
+        printf("%d", n%2);
+    }
+    
+}
 
 int main(){
 
@@ -39,6 +87,19 @@ int main(){
     // int tam = 37;
 
     // naturais(tam);
+
+    // int n = 8;
+
+    // for(int i = 0; i < n; i++){
+    //     printf("%d ", fibonatti(i));
+    // }
+
+    // printf("%d", Ackermann(3, 0));
+
+    // printf("%d", inverso(234, 0));
+
+    binario(5);
+    
 
     return 0;
 }
