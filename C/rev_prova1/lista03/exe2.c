5094acb9d828cfba3d04d896f34b97125e180830
Stephany Resque <resquestephany@gmail.com>
2024-09-25 14:40:55 -0300

    exe2

--- C/rev_prova1/lista03/exe2.c
+++ C/rev_prova1/lista03/exe2.c
@@ -1,1 +1,18 @@
-#include
+#include <stdio.h>
+
+int sequencia(int n){
+
+    if(n == 0){
+        return 1;
+    }else{
+        sequencia(n-1);
+        printf("%d ", n);
+    }
+}
+
+int main(){
+
+    sequencia(37);
+
+    return 0;
+}
