f76aedd5b3395e10c2da7772ea40018610610863
Stephany Resque <resquestephany@gmail.com>
2024-10-25 11:35:26 -0300

    r

--- C/listas_unidade_03/lista02/lista/dinamica/exe02/lista.c
+++ C/listas_unidade_03/lista02/lista/dinamica/exe02/lista.c
@@ -54,3 +54,85 @@ int inserir_inicio(Lista *li, Numero number){
 
     return 1;
 }
+
+int inserir_final(Lista *li, Numero number){
+    if(li == NULL){
+        return 0;
+    }
+
+    Elem *no = (Elem*)malloc(sizeof(Elem));
+    if(no == NULL){
+        return 0;
+    }
+
+    no->number = number;
+    no->prox = NULL;
+
+    if((*li) == NULL){
+        no->ant = NULL;
+        (*li) = no;
+    }else{
+        Elem *aux = (*li);
+        while(aux->prox != NULL){
+            aux = aux->prox;
+        }
+
+        aux->prox = no;
+        no->ant = aux;
+    }
+
+    return 1;
+}
+
+int inserir_pos(Lista *li, int pos,Numero number){
+    if(li == NULL || pos <= 0){
+        return 0;
+    }
+
+    Elem *no = (Elem*)malloc(sizeof(Elem));
+    if(no == NULL){
+        return 0;
+    }
+
+    no->number = number;
+
+    if(pos == 1){
+        no->prox = (*li);
+        no->ant = NULL;
+        if (*li != NULL) {
+            (*li)->ant = no;
+        }
+        (*li) = no;
+
+        return 1;
+    }
+
+    Elem *aux = (*li);
+    int i = 1;
+    while(aux != NULL && i < pos - 1){
+        aux = aux->prox;
+        i++;
+    }
+
+    if(aux == NULL){
+        return 0;
+    }
+
+    no->prox = aux->prox;
+    no->ant = aux;
+
+    
+}
+
+void imprime_lista(Lista *li) {
+    if (li == NULL || (*li) == NULL) {
+        printf("Lista vazia.\n");
+        return;
+    }
+    Elem *no = (*li);
+    while (no != NULL) {
+        printf("%d ", no->number.valor);
+        no = no->prox;
+    }
+    printf("\n");
+}
--- C/listas_unidade_03/lista02/lista/dinamica/exe02/lista.h
+++ C/listas_unidade_03/lista02/lista/dinamica/exe02/lista.h
@@ -9,3 +9,5 @@ Lista* cria_lista();//
 void libera_lista(Lista *li);//
 int inserir_inicio(Lista *li, Numero number);//
 int inserir_final(Lista *li, Numero number);//
+int inserir_pos(Lista *li, int pos,Numero number);//
+void imprime_lista(Lista *li);//
