e72d26fcee7598d09a4a4a57d9cf7d6b4b83188c
Stephany Resque <resquestephany@gmail.com>
2024-11-11 13:35:24 -0300

    a

---
+++ C/listas_unidade_04/estatica/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
+
+ int main() {
+    clock_t inicio, fim;
+    unsigned long int tempo;
+    
+    inicio = clock();
+    /* coloque seu código aqui */
+    fim = clock();
+    
+    tempo = (fim- inicio) * 1000 / CLOCKS_PER_SEC;
+    printf("tempo: %lu milissegundo\n", tempo);
+    return 0;
+ }
