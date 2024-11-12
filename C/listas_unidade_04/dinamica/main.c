16df50df5846f034ae11ee8137dc61cc3e58d4eb
Stephany Resque <resquestephany@gmail.com>
2024-11-12 09:25:26 -0300

    Update main.c

--- C/listas_unidade_04/dinamica/main.c
+++ C/listas_unidade_04/dinamica/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
- #include <stdlib.h>
- #include <time.h>
+#include <stdlib.h>
+#include <time.h>
 
- int main() {
-    clock_t inicio, fim;
-    unsigned long int tempo;
-    
-    inicio = clock();
-    /* coloque seu código aqui */
-    fim = clock();
-    
-    tempo = (fim- inicio) * 1000 / CLOCKS_PER_SEC;
-    printf("tempo: %lu milissegundo\n", tempo);
-    return 0;
- }
+int main() {
+   clock_t inicio, fim;
+   unsigned long int tempo;
+      
+   inicio = clock();
+   /* coloque seu código aqui */
+   fim = clock();
+      
+   tempo = (fim- inicio) * 1000 / CLOCKS_PER_SEC;
+   printf("tempo: %lu milissegundo\n", tempo);
+   return 0;
+}
